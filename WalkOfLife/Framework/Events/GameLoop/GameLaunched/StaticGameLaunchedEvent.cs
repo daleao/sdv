cefaacd0af6d711fa38d14d77324e78175af4c79
TehPers <tehperz@gmail.com>
2021-12-24 23:27:00 -0800

    Add Teh's Fishing Overhaul integration to WoL

--- WalkOfLife/Framework/Events/GameLoop/GameLaunched/StaticGameLaunchedEvent.cs
+++ WalkOfLife/Framework/Events/GameLoop/GameLaunched/StaticGameLaunchedEvent.cs
@@ -4,25 +4,32 @@ using TheLion.Stardew.Professions.Integrations;
 
 namespace TheLion.Stardew.Professions.Framework.Events
 {
-	[UsedImplicitly]
-	internal class StaticGameLaunchedEvent : GameLaunchedEvent
-	{
-		/// <inheritdoc />
-		public override void OnGameLaunched(object sender, GameLaunchedEventArgs e)
-		{
-			// add Generic Mod Config Menu integration
-			new GenericModConfigMenuIntegrationForAwesomeTools(
-				getConfig: () => ModEntry.Config,
-				reset: () =>
-				{
-					ModEntry.Config = new();
-					ModEntry.ModHelper.WriteConfig(ModEntry.Config);
-				},
-				saveAndApply: () => { ModEntry.ModHelper.WriteConfig(ModEntry.Config); },
-				log: ModEntry.Log,
-				modRegistry: ModEntry.ModHelper.ModRegistry,
-				manifest: ModEntry.Manifest
-			).Register();
-		}
-	}
+    [UsedImplicitly]
+    internal class StaticGameLaunchedEvent : GameLaunchedEvent
+    {
+        /// <inheritdoc />
+        public override void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // add Generic Mod Config Menu integration
+            new GenericModConfigMenuIntegrationForAwesomeTools(
+                getConfig: () => ModEntry.Config,
+                reset: () =>
+                {
+                    ModEntry.Config = new();
+                    ModEntry.ModHelper.WriteConfig(ModEntry.Config);
+                },
+                saveAndApply: () => { ModEntry.ModHelper.WriteConfig(ModEntry.Config); },
+                log: ModEntry.Log,
+                modRegistry: ModEntry.ModHelper.ModRegistry,
+                manifest: ModEntry.Manifest
+            ).Register();
+
+            // add Teh's Fishing Overhaul integration
+            if (ModEntry.ModHelper.ModRegistry.IsLoaded("TehPers.FishingOverhaul"))
+            {
+                new TehsFishingOverhaulIntegration(modRegistry: ModEntry.ModHelper.ModRegistry, log: ModEntry.Log)
+                    .Register();
+            }
+        }
+    }
 }
