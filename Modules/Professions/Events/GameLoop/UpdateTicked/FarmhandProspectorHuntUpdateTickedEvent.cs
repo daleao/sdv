a4d3278bd3209be6d2e436dbeefe70e7969ab002
Mat L <2775342-daleao@users.noreply.gitlab.com>
2023-11-22 16:12:55 -0300

    4.1.5-finall

--- Modules/Professions/Events/GameLoop/UpdateTicked/FarmhandProspectorHuntUpdateTickedEvent.cs
+++ Modules/Professions/Events/GameLoop/UpdateTicked/FarmhandProspectorHuntUpdateTickedEvent.cs
@@ -2,9 +2,13 @@
 
 #region using directives
 
+using System.Collections.Generic;
+using System.Linq;
 using DaLion.Overhaul.Modules.Professions.TreasureHunts;
 using DaLion.Overhaul.Modules.Professions.VirtualProperties;
 using DaLion.Shared.Events;
+using DaLion.Shared.Extensions;
+using DaLion.Shared.Extensions.Stardew;
 using StardewModdingAPI.Events;
 
 #endregion using directives
@@ -13,6 +17,7 @@ using StardewModdingAPI.Events;
 internal sealed class FarmhandProspectorHuntUpdateTickedEvent : UpdateTickedEvent
 {
     private ProspectorHunt? _hunt;
+    private IEnumerable<SObject> _stonesPrevious = null!; // set when enabled
 
     /// <summary>Initializes a new instance of the <see cref="FarmhandProspectorHuntUpdateTickedEvent"/> class.</summary>
     /// <param name="manager">The <see cref="EventManager"/> instance that manages this event.</param>
@@ -25,6 +30,7 @@ internal sealed class FarmhandProspectorHuntUpdateTickedEvent : UpdateTickedEven
     protected override void OnEnabled()
     {
         this._hunt ??= Game1.player.Get_ProspectorHunt();
+        this._stonesPrevious = Game1.player.currentLocation.Objects.Values.Where(o => o.IsStone()).ToList();
     }
 
     /// <inheritdoc />
@@ -41,10 +47,38 @@ internal sealed class FarmhandProspectorHuntUpdateTickedEvent : UpdateTickedEven
             return;
         }
 
-        if (Game1.player.currentLocation.Objects.ContainsKey(this._hunt.TreasureTile.Value))
+        if (!Game1.player.currentLocation.Objects.ContainsKey(this._hunt.TreasureTile.Value))
+        {
+            this._hunt.Complete();
+            this.Disable();
+            return;
+        }
+
+        if (ProfessionsModule.Config.UseLegacyProspectorHunt)
         {
             return;
         }
+
+        var location = Game1.player.currentLocation;
+        var stonesCurrent = location.Objects.Values.Where(o => o.IsStone()).ToList();
+        var removed = this._stonesPrevious.Except(stonesCurrent).SingleOrDefault();
+        if (removed is null)
+        {
+            this._stonesPrevious = stonesCurrent;
+            return;
+        }
+
+        var distanceToTreasure = (int)removed.DistanceTo(this._hunt!.TreasureTile.Value);
+        var detectionDistance = (int)ProfessionsModule.Config.ProspectorDetectionDistance;
+        if (detectionDistance > 0 && !distanceToTreasure.IsIn(1..detectionDistance))
+        {
+            this._stonesPrevious = stonesCurrent;
+            return;
+        }
+
+        var pitch = (int)(2400f * (1f - ((float)distanceToTreasure / detectionDistance)));
+        Game1.playSoundPitched("detector", pitch);
+        Log.A($"Beeped at frequency {pitch} Hz");
 
         this._hunt.Complete();
         this.Disable();
