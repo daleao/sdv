ef4ad568ecad7010ce20dc4100d484a54a980dff
Cameron Labelle <labelle.cam3@gmail.com>
2026-04-11 21:08:50 -0400

    Fix trinkets from targeting piped slimes

---
+++ Professions/Framework/Patchers/Combat/FindClosestMonsterPatcher.cs
@@ -0,0 +1,42 @@
+﻿namespace DaLion.Professions.Framework.Patchers.Combat;
+
+#region using directives
+
+using DaLion.Professions.Framework.VirtualProperties;
+using DaLion.Shared.Harmony;
+using HarmonyLib;
+using StardewValley.Monsters;
+
+#endregion
+
+[UsedImplicitly]
+internal sealed class FindClosestMonsterPatcher : HarmonyPatcher
+{
+    internal FindClosestMonsterPatcher(Harmonizer harmonizer, Logger logger)
+        : base(harmonizer, logger)
+    {
+        this.Target = this.RequireMethod<Utility>(nameof(Utility.findClosestMonsterWithinRange));
+    }
+
+    #region harmony patches
+
+    [HarmonyPrefix]
+    [UsedImplicitly]
+    private static bool FindClosestMonsterPrefix(ref Func<Monster, bool> match)
+    {
+        Func<Monster, bool> filter = (monster) => monster is GreenSlime slime && slime.Get_Piped() == null;
+        if (match == null)
+        {
+            match = filter;
+        }
+        else
+        {
+            var prev = match;
+            match = (monster) => prev(monster) && filter(monster);
+        }
+
+        return true;
+    }
+
+    #endregion harmony patches
+}
